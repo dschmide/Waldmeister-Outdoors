3c22fd315e8ee55118f301416a3b57c477112c13
Daniel Schmider <daniel.schmider@hsr.ch>
2018-03-30 21:20:22 +0200

    Login + Register error message and enter

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -9,6 +9,7 @@
         <v-text-field
               label="Displayname"
               v-model="displayname"
+              @keyup.enter="register" 
         ></v-text-field>
         
         <br>
@@ -16,14 +17,22 @@
               label="Password"
               type="password"
               v-model="password"
+              @keyup.enter="register" 
         ></v-text-field>
         <br>
         <v-text-field
               label="Email"
               v-model="email"
+              @keyup.enter="register" 
         ></v-text-field>
-        <div class="error" v-html="error" />
-        <br>
+        <div class="error" v-if="error">
+          <ul>
+            <li v-if="error.data.username">Displayname: {{ error.data.username[0] }}</li>
+            <li v-if="error.data.password">Password: {{ error.data.password[0] }}</li>
+            <li v-if="error.data.email">Email: {{ error.data.email[0] }}</li>
+            <li v-if="error.data">{{ error.data[0] }}</li>
+          </ul>
+        </div><br>
         <v-btn
           dark
           class="light-green"
@@ -55,10 +64,6 @@ export default {
           username: this.displayname
         })
         .then( () => {
-        // } catch (error) {
-        //   this.error = error.response.data.error
-        // }
-
           const Response = AuthenticationSevice.login({ 
             username: this.displayname, 
             password: this.password
@@ -72,14 +77,21 @@ export default {
             console.log(response.data)
           })
         })
+        .catch((error) => {
+          this.error = error.response;
+          console.log("err");
+        })
       }
     }
   }
 </script>
 
 <style scoped>
-.error{
-   color:red;
- }
+
+.error {
+  color: white;
+  list-style:  none;
+  text-align: left;
+}
 
 </style>
