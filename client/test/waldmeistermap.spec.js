b9967a23835b59f1b95808a6ac45c51b1883e008
Daniel Schmider <daniel.schmider@hsr.ch>
2018-04-03 21:27:29 +0200

    Testsframework for Frontend VueJS Components

---
+++ client/test/waldmeistermap.spec.js
@@ -0,0 +1,13 @@
+import { shallow } from '@vue/test-utils'
+//import Vuex from 'vuex'
+import Waldmeistermap from '../src/components/Waldmeistermap.vue'
+
+//const localVue = createLocalVue()
+
+//localVue.use(Vuex)
+
+describe('Waldmeistermap.vue', () => {
+  it('sample test', () => {
+    const wrapper = shallow(Waldmeistermap)
+  })
+})
