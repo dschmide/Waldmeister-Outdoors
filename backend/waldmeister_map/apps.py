2018c4f64eb71fa1a5ed860ab21ca01de7de20cf
Nicola Jordan <nic@hixi.ch>
2018-04-17 16:51:57 +0200

    flake corrections and removal of dead code

--- backend/waldmeister_map/apps.py
+++ backend/waldmeister_map/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
 
+
 class WaldmeistermapConfig(AppConfig):
     name = 'waldmeister_map'
--- backend/waldmeister_map/urls.py
+++ backend/waldmeister_map/urls.py
@@ -1,50 +1,27 @@
-#urls.py in waldmeister_map
-
-from django.conf.urls import url, include
+from django.conf.urls import url
+from django.urls import include
 from rest_framework import routers
-#from waldmeister_map import views
-from . import views
-
-#Djoser
-from django.urls import include, re_path
-
-#Swagger
 from rest_framework_swagger.views import get_swagger_view
 
-
+from . import views
 
 router = routers.DefaultRouter()
-# router.register(r'users', views.UserViewSet)
-# router.register(r'groups', views.GroupViewSet)
-#router.register(r'register', views.register)
 router.register(r'areas', views.UserAreaViewSet)
 
-#swagger
 schema_view = get_swagger_view(title='Waldmeister API')
 
 
 urlpatterns = [
-	#/waldmeister_map/
     url(r'^$', views.index, name='index'),
 
-    #Rest test
-	url(r'^api/', include(router.urls)),
+    # Rest test
+    url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    #waldmeister_map/map
-    #url(r'^/Map/', views.map, name='map')
 
-    #url(r'^register', views.register, name='register'),
-
-    #url(r'^rest-auth/', include('rest_auth.urls')),
-	#JWT Token
-    #url(r'^api-token-auth/', obtain_jwt_token),
-
-    #Djoser
-    #re_path(r'^api/', include('djoser.urls')),
     url(r'^auth/', include('djoser.urls')),
     url(r'^auth/', include('djoser.urls.authtoken')),
     url(r'^auth/', include('djoser.urls.jwt')),
-    
-    #swagger
+
+    # swagger
     url(r'^swagger', schema_view)
 ]
